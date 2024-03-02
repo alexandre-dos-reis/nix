22aabff5f57b32aa156ae8b8d4672d5179cc7239
Alexandre Dos Reis <alexandre@finishers.com>
2024-03-02 21:47:24 +0100

    disabling keychain in linux

--- home/alex/programs/keychain.nix
+++ home/alex/programs/keychain.nix
@@ -1,6 +1,7 @@
 {globals, ...}: {
   programs.keychain = {
-    enable = globals.utils.isLinux;
+    # enable = globals.utils.isLinux;
+    enable = false; # Need to try fish plugin first on linux...
     enableFishIntegration = true;
     keys = [
       /home/${globals.username}/.ssh/id_ed25519
