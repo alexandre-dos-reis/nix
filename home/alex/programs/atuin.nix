c1d21e54f3033f532fe44302027a717b8d7991fe
Alexandre Dos Reis <ajm.dosreis.daponte@gmail.com>
2024-08-06 15:54:55 +0200

    atuin: up key disable

--- home/alex/programs/atuin.nix
+++ home/alex/programs/atuin.nix
@@ -2,8 +2,12 @@
   programs.atuin = {
     enable = true;
     enableFishIntegration = true;
+    flags = [
+      "--disable-up-arrow"
+    ];
     settings = {
       keymap_mode = "vim-insert";
+      filter_mode_shell_up_key_binding = "session";
       keys = {
         scroll_exits = false;
       };
