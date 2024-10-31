83f09fe3200aef637e79342e7b126691e48d8201
Alexandre Dos Reis <ajm.dosreis.daponte@gmail.com>
2024-10-31 18:28:33 +0100

    todo TS parsers

--- home/alex/programs/neovim.nix
+++ home/alex/programs/neovim.nix
@@ -17,7 +17,9 @@
   # };
 
   # https://github.com/nix-community/nixd/blob/main/nixd/docs/user-guide.md#configuration
-  home.packages = with pkgs; [
+  home.packages = with pkgs;
+  with pkgs.tree-sitter-grammars; [
+    # LSPs
     # Javascript / Typescript
     typescript-language-server # Find a way to install vtsls
     typescript
@@ -35,5 +37,31 @@
     # Nix
     nixd
     alejandra # formatter
+
+    # TREESITTER PARSERS
+    tree-sitter-typescript
+
+    # TODO: Implement theses parsers
+    # "html",
+    # "lua",
+    # "tsx",
+    # "javascript",
+    # "go",
+    # "astro",
+    # "cmake",
+    # "css",
+    # "scss",
+    # "fish",
+    # "gitignore",
+    # "markdown",
+    # "graphql",
+    # "http",
+    # "php",
+    # "rust",
+    # "sql",
+    # "nix",
+    # "just",
+    # "terraform",
+    # "zig",
   ];
 }
