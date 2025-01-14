0aa67b8da3259e1cc3912178c480114baf168d64
Alexandre Dos Reis <ajm.dosreis.daponte@gmail.com>
2025-01-14 22:13:15 +0100

    nushell working config

--- home/alex/programs/shells/nushell.nix
+++ home/alex/programs/shells/nushell.nix
@@ -10,13 +10,47 @@ in {
     enable = true;
 
     extraConfig = ''
-      $env.config.show_banner = false
-      $env.config.buffer_editor = "nvim"
-      $env.config.edit_mode = "vi"
-      $env.PROMPT_INDICATOR_VI_NORMAL = "n "
-      $env.TRANSIENT_PROMPT_INDICATOR_VI_NORMAL = "n "
-      $env.PROMPT_INDICATOR_VI_INSERT = "> "
-      $env.TRANSIENT_PROMPT_INDICATOR_VI_INSERT = "> "
+
+       let fish_completer = {|spans|
+           fish --command $'complete "--do-complete=($spans | str join " ")"'
+           | from tsv --flexible --noheaders --no-infer
+           | rename value description
+       }
+
+
+      # This completer will use carapace by default
+       let external_completer = {|spans|
+           let expanded_alias = scope aliases
+           | where name == $spans.0
+           | get -i 0.expansion
+
+           let spans = if $expanded_alias != null {
+               $spans
+               | skip 1
+               | prepend ($expanded_alias | split row ' ' | take 1)
+           } else {
+               $spans
+           }
+
+           match $spans.0 {
+               _ => $fish_completer
+           } | do $in $spans
+       }
+
+       $env.PROMPT_INDICATOR_VI_NORMAL = "n "
+       $env.TRANSIENT_PROMPT_INDICATOR_VI_NORMAL = "n "
+       $env.PROMPT_INDICATOR_VI_INSERT = "> "
+       $env.TRANSIENT_PROMPT_INDICATOR_VI_INSERT = "> "
+
+       $env.config.show_banner = false
+       $env.config.buffer_editor = "nvim"
+       $env.config.edit_mode = "vi"
+
+       $env.config.completions.external = {
+         enable: true
+         completer: $external_completer
+
+       }
     '';
 
     shellAliases = {
