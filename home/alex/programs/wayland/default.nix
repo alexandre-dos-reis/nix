ae5e7cf7dccbbdc49b51b226e91e2d9ba702ded0
Alexandre Dos Reis <ajm.dosreis.daponte@gmail.com>
2025-02-28 22:35:23 +0100

    home: wofi & waybar

--- home/alex/programs/wayland/default.nix
+++ home/alex/programs/wayland/default.nix
@@ -1,8 +1,18 @@
-{
+{pkgs, ...}: {
   # TODO: https://www.youtube.com/watch?v=zt3hgSBs11g
   # For now just tweak the config file in `~/.config/hypr/hyprland.conf`
 
   # wayland.windowManager.hyprland = {
   #   enable = true;
   # };
+
+  # App launcher
+  programs.wofi.enable = true;
+  # Status bar
+  programs.waybar.enable = true;
+
+  # TUI-based clipboard manager
+  home.packages = [
+    pkgs.clipse
+  ];
 }
