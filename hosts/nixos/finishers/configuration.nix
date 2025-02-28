9be457d22f8b14698b351a3f54ba8dd4dc582613
Alexandre Dos Reis <ajm.dosreis.daponte@gmail.com>
2025-02-28 13:55:59 +0100

    hyprland enabled !

--- hosts/nixos/finishers/configuration.nix
+++ hosts/nixos/finishers/configuration.nix
@@ -47,10 +47,12 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+
+  # Wayland
+  programs.hyprland.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
