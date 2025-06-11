054c55d8ea6bd3a0b7f4f510858cbf2e5e88c06c
Alexandre Dos Reis <ajm.dosreis.daponte@gmail.com>
2025-06-11 13:28:45 +0200

    moved shell aliases to user

--- home/alex/programs/nushell/default.nix
+++ home/alex/programs/nushell/default.nix
@@ -49,35 +49,17 @@ in {
         ${mkAutopair "AutoSinglequote" "'" "'"}
       ]
     '';
-    shellAliases = {
-      v = "nvim";
-      t = "tmux";
-      ta = "tmux attach";
-      tk = "tmux kill-session";
-      ll = "ls -l";
-      lla = "ls -la";
-      c = "clear";
-      b = "bat -p";
-      cs = "~/bin/cht.sh";
-      ff = "fzf --preview 'bat --style=numbers --color=always {}'";
-      ffn = "ff | xargs nvim";
-      s = "sudo env PATH=\"$PATH\"";
-
-      d = "docker";
-      dcp = "docker compose";
-
-      k = "kubectl";
-      kns = "kubens";
-      kx = "kubectx";
-
-      g = "git";
-      gst = "git status";
-      ga = "git add";
-      gcm = "git commit -m";
-      gco = "git checkout";
-      gcb = "git checkout -b";
-      gp = "git push";
-      gl = "git pull";
-    };
+    shellAliases =
+      user.shellAliases
+      // {
+        g = "git";
+        gst = "git status";
+        ga = "git add";
+        gcm = "git commit -m";
+        gco = "git checkout";
+        gcb = "git checkout -b";
+        gp = "git push";
+        gl = "git pull";
+      };
   };
 }
