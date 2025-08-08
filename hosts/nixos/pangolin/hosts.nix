db02305c7724132b349e7e64bb32034991171d32
Alexandre Dos Reis <ajm.dosreis.daponte@gmail.com>
2025-08-08 16:00:43 +0200

    added email host

--- hosts/nixos/pangolin/hosts.nix
+++ hosts/nixos/pangolin/hosts.nix
@@ -1,14 +1,17 @@
 {
   networking.extraHosts = let
     domain = "dev.eurorack-3d.com";
-    subDomains = ["" "api" "admin" "assets" "minio"];
   in
     toString (map (subDomain: "127.0.0.1 ${
         if subDomain == ""
         then domain
         else "${subDomain}.${domain}"
       }\n")
-      subDomains);
+      ["" "api" "admin" "assets" "minio" "email"]);
 
+  # For mkcert generate run:
+  # - mkcert -install in ~/.local/share/mkcert
+  # It will generate a public and private root certificate authority keys.
+  # - import the rootCa here, don't `git add` the rootCA-key.pem file, it's the private key
   security.pki.certificateFiles = [./certificates/rootCA.pem];
 }
